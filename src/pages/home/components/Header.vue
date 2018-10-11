e2bae2f00b96c90266bc7da0929b5608f600dac5
liujie <1509582688@qq.com>
2018-10-12 00:40:19 +0800

    'thrid'

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市景点
+        </div>
+        <div class="header-right">
+            <span class="iconfont arrow-icon">&#xe737;</span>
+            城市
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~style/varibles.styl'
+    .header
+        display: flex
+        height: .86rem
+        background: $bgcolor
+        line-height: .86rem
+        color: #fff
+    
+    .header-left
+        width: .64rem
+        float: left
+        .back-icon
+            text-align:center
+            font-size: .4rem
+    
+    .header-right
+        width: 1.24rem
+        float: right
+        text-align: center
+        .arrow-icon
+            font-size: .24rem
+            margin-left: -.04rem 
+    
+    .header-input
+        flex: 1
+        background: #ffffff
+        border-radius: .1rem
+        height: .64rem
+        line-height: .64rem
+        margin-top: .12rem
+        margin-left: .2rem
+        color: #ccc
+        padding-left:.2rem  
+</style>
