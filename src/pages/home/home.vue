4b6ba72b076ab5fd51a781be28383ae58ff0e096
liujie <1509582688@qq.com>
2018-10-11 08:41:28 +0800

    更新

---
+++ src/pages/home/home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="home">Home</div>
+        <router-link to="/List" class="home">列表页</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Home"
+}
+</script>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+}
+
+.home{
+    font-size: 50px;
+}
+</style>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Home from '@/pages/home/home'
+import List from '@/pages/List/List'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path:'/List',
+      name:'List',
+      component:List
     }
   ]
 })
