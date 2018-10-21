d74ec6504e1fbdd5e1e0bc6223dda15d0d932ed9
liujie <1509582688@qq.com>
2018-10-21 15:22:54 +0800

    'search'

---
+++ src/pages/city/components/search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="search">
+        <input type="text" class="search-input" placeholder="输出城市名或拼音">
+    </div>
+</template>
+
+<script>
+export default {
+    name:'CitySearch'    
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~style/varibles.styl'
+    .search
+        height :.72rem
+        padding: .1rem
+        background :$bgcolor
+        .search-input 
+            box-sizing:border-box 
+            height :.62rem
+            line-height :.62rem
+            width :100%
+            padding :0 .1rem
+            text-align :center
+            border-radius :.06rem 
+            color :#666
+</style>
