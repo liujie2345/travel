c64644efec7b223b880a305de8212bad89c640e7
liujie <1509582688@qq.com>
2018-10-22 20:54:10 +0800

    'use-vuex'

--- src/pages/city/components/list.vue
+++ src/pages/city/components/list.vue
@@ -5,7 +5,7 @@
                 <div class="title border-topbottom">当前城市</div>
                 <div class="button-list">
                     <div class="button-wrapper">
-                        <div class="button">北京</div>
+                        <div class="button">{{this.currentCity}}</div>
                     </div>
                 </div>
             </div>
@@ -13,20 +13,21 @@
             <div class="area">
                 <div class="title border-topbottom">热门城市</div>
                 <div class="button-list">
-                    <div class="button-wrapper" v-for="item of hot" :key="item.id">
+                    <div class="button-wrapper" v-for="item of hot" :key="item.id" 
+                    @click="handleCityClick(item.name)">
                         <div class="button">{{item.name}}</div>
                     </div>
                 </div>
             </div>
         
-            <div class="area" v-for="(item,key) of cities" :key="key">
+            <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
                 <div class="title border-topbottom">{{key}}</div>
                 <div class="item-list">
                     <div 
                     class="item border-bottom" 
                     v-for="innerItem of item" 
                     :key="innerItem.id"
-                    :ref="key"
+                    @click="handleCityClick(innerItem.name)"
                     >
                     {{innerItem.name}}</div>
                 </div>
@@ -37,6 +38,7 @@
 
 <script>
 import Bscroll from 'better-scroll'
+import {mapState,mapMutations} from 'vuex'
 export default {
     name:"CityList",
     mounted() {
@@ -54,6 +56,19 @@ export default {
                 this.scroll.scrollToElement(element)
             }
         }
+    },
+    methods:{
+        handleCityClick (city){
+            // this.$store.dispatch('changeCity',city)
+            this.changeCity(city)
+            this.$router.push('/')
+        },
+        ...mapMutations(['changeCity'])
+    },
+    computed:{
+        ...mapState({
+            currentCity:'city'
+        })
     }
 }
 </script>
